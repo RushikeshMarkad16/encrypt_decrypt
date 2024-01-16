73fdb4c22bbf965185338b969e3327b4b5ed08d3
Rushikesh Markad <rushikesh.markad@joshsoftware.com>
2024-01-16 18:01:10 +0530

    added encryption decryption

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"time"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 func generateRSAPrivateKey() *rsa.PrivateKey {
 	// Generate a new RSA private key with 2048 bits
@@ -52,35 +64,164 @@ func encodePublicKey(publicKey *rsa.PublicKey, filename string) {
 	publicKeyFile.Close()
 }
 
-func encrypt(publicKey *rsa.PublicKey, plaintext []byte) ([]byte, error) {
-	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, plaintext)
+func aesEncrypt(key, plaintext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
+
+	// Padding the plaintext to a multiple of the block size
+	padding := aes.BlockSize - (len(plaintext) % aes.BlockSize)
+	paddedText := append(plaintext, bytes.Repeat([]byte{byte(padding)}, padding)...)
+
+	ciphertext := make([]byte, aes.BlockSize+len(paddedText))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], paddedText)
+
 	return ciphertext, nil
 }
 
-func decrypt(privateKey *rsa.PrivateKey, ciphertext []byte) (string, error) {
-	plaintext, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, ciphertext)
+func aesDecrypt(key, ciphertext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(plaintext), nil
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	// Remove padding
+	padding := int(ciphertext[len(ciphertext)-1])
+	return ciphertext[:len(ciphertext)-padding], nil
+}
+
+func encryptAndEncode(publicKey2 *rsa.PublicKey, message []byte) string {
+	// Generate a random symmetric key for AES encryption
+	symmetricKey := make([]byte, 32) // 32 bytes for AES-256
+	if _, err := rand.Read(symmetricKey); err != nil {
+		panic(err)
+	}
+
+	// Encrypt the symmetric key with the RSA public key
+	encryptedSymmetricKey, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey2, symmetricKey)
+	if err != nil {
+		panic(err)
+	}
+
+	// Encrypt the actual data with the symmetric key using AES
+	ciphertext, err := aesEncrypt(symmetricKey, message)
+	if err != nil {
+		panic(err)
+	}
+
+	// Combine the encrypted symmetric key and the AES-encrypted data
+	combined := append(encryptedSymmetricKey, ciphertext...)
+
+	// Base64 encode and print the result
+	encodedResult := base64.StdEncoding.EncodeToString(combined)
+
+	return encodedResult
+}
+
+func decryptAndDecode(privateKey *rsa.PrivateKey, encodedResult string) []byte {
+	// Decrypt and decode
+	decodedResult, err := base64.StdEncoding.DecodeString(encodedResult)
+	if err != nil {
+		panic(err)
+	}
+
+	decryptedSymmetricKey, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, decodedResult[:privateKey.Size()])
+	if err != nil {
+		panic(err)
+	}
+
+	decryptedMessage, err := aesDecrypt(decryptedSymmetricKey, decodedResult[privateKey.Size():])
+	if err != nil {
+		panic(err)
+	}
+
+	return decryptedMessage
+}
+
+func configDB() {
+
+	// Connection parameters
+	connStr := "user=postgres dbname=test1 password=postgres sslmode=disable"
+	var err error
+	// Open a database connection
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to the database")
+
+}
+
+func storeToDb(encodedResult string) {
+	// Perform an insert operation
+	insertQuery := `INSERT INTO encode (encoded_value,timestamp) VALUES ($1,$2)`
+	result, err := db.Exec(insertQuery, encodedResult, time.Now())
+	if err != nil {
+		fmt.Println("Error inserting data : ", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Inserted %d rows\n", rowsAffected)
+}
+
+type EncodeData struct {
+	EncodedValue string
+	Timestamp    time.Time
+}
+
+func GetFromDb() EncodeData {
+	selectQuery := `SELECT * FROM encode ORDER BY timestamp DESC LIMIT 1;`
+	rows, err := db.Query(selectQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var encodeData EncodeData
+
+	for rows.Next() {
+		err := rows.Scan(&encodeData.EncodedValue, &encodeData.Timestamp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return encodeData
+
 }
 
 func main() {
-	// privateKey1 := generateRSAPrivateKey()
-	// encodePrivateKey(privateKey1, "private_key1.pem")
-
+	configDB()
+	defer db.Close()
 	privateKey2 := generateRSAPrivateKey()
 	encodePrivateKey(privateKey2, "private_key2.pem")
 	publicKey2 := &privateKey2.PublicKey
 
-	fmt.Println("size : ", publicKey2.Size())
-
 	encodePublicKey(publicKey2, "public_key2.pem")
 
-	msg := []byte(`-----BEGIN RSA PRIVATE KEY-----
+	message := []byte(`-----BEGIN RSA PRIVATE KEY-----
 	MIIEowIBAAKCAQEAt7arlt5MBvdM3B98K84JkpSg8KGbRXi9FtPTFzH8mYK1vdZ0
 	kkMyOzNVIQU8V2wUu4zGvC1qXoeBSZDvZavzZHSNtO3rQMp8uoNVKs/DD7ZprCkB
 	AjfWEmtP1qhbZBiLMHaufpSREyXV6FcZukvudent4vPl2ie8gDQqbXH0EBloROEG
@@ -108,33 +249,15 @@ func main() {
 	QvJRzj1ree7m7kyNl+a8H7G/vUvU2t8tXt3Q26FTuaDg2vzzxfPm
 	-----END RSA PRIVATE KEY-----`)
 
-	// msg := []byte("This is a sample RSA PRIVATE KEY")
+	encodedResult := encryptAndEncode(publicKey2, message)
 
-	// Encrypt with public key 1
-	ciphertext, err := encrypt(publicKey2, msg)
-	if err != nil {
-		fmt.Println("error : ", err)
-		return
-	}
-	fmt.Println("ciphertext : ", ciphertext)
+	// Insert to DB
+	storeToDb(encodedResult)
 
-	encodedCiphertext := base64.StdEncoding.EncodeToString(ciphertext)
+	// Get from database
+	encodeData := GetFromDb()
 
-	fmt.Println("encodedCiphertext : ", encodedCiphertext)
+	decryptedMessage := decryptAndDecode(privateKey2, encodeData.EncodedValue)
 
-	// Base64 decode
-	decodedCiphertext, err := base64.StdEncoding.DecodeString(encodedCiphertext)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("decodedCiphertext : ", decodedCiphertext)
-
-	// Decrypt with private key 1
-	decryptedMessage, err := decrypt(privateKey2, decodedCiphertext)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("decryptedMessage : ", decryptedMessage)
-
+	fmt.Println("Decrypted message:", string(decryptedMessage))
 }
